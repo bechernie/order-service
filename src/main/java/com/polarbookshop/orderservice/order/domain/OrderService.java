07c5aaf06c0ade515cb4a1c29ac8273ff551b533
bechernie <bechernie@gmail.com>
2024-02-04 15:32:08 +0100

    order service calls catalog service

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.orderservice.order.domain;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,20 +10,29 @@ import reactor.core.publisher.Mono;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final BookClient bookClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
         this.orderRepository = orderRepository;
+        this.bookClient = bookClient;
+    }
+
+    private static Order buildAcceptedOrder(Book book, int quantity) {
+        return Order.of(book.isbn(), book.title() + "-" + book.author(), book.price(), quantity, OrderStatus.ACCEPTED);
+    }
+
+    private static Order buildRejectedOrder(String isbn, int quantity) {
+        return Order.of(isbn, null, null, quantity, OrderStatus.REJECTED);
     }
 
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
-    public Mono<Order> submitOrder(String isbn,int quantity) {
-        return Mono.just(buildRejectedOrder(isbn,quantity)).flatMap(orderRepository::save);
-    }
-
-    private static Order buildRejectedOrder(String isbn, int quantity) {
-        return Order.of(isbn,null,null,quantity, OrderStatus.REJECTED);
+    public Mono<Order> submitOrder(String isbn, int quantity) {
+        return bookClient.getBookByIsbn(isbn)
+                .map(book -> buildAcceptedOrder(book, quantity))
+                .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
+                .flatMap(orderRepository::save);
     }
 }
--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE orders
 (
     id                 BIGSERIAL PRIMARY KEY NOT NULL,
     book_isbn          VARCHAR(255)          NOT NULL,
-    book_name          VARCHAR(255)          NOT NULL,
+    book_name          VARCHAR(255),
     book_price         FLOAT8,
     quantity           INT                   NOT NULL,
     status             VARCHAR(255)          NOT NULL,
